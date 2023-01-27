7706c2a8d2c72fcd181a7d7a5ae4957bb9f79d70
SeanUlep <su25@njit.edu>
2023-01-27 01:45:30 -0500

    Some Funny Additions

--- web/src/pages/AboutPage/AboutPage.js
+++ web/src/pages/AboutPage/AboutPage.js
@@ -6,7 +6,10 @@ const AboutPage = () => {
       <MetaTags title="About" description="About page" />
 
       <p>I am actually very amazing. Yes, me, Sean Ulep.</p>
-
+      <img
+        src="https://cdn.discordapp.com/attachments/932835514099314730/1068420503200608276/JJBA-IT-WAS-ME-DIO_1.jpg"
+        alt="Me, Sean"
+      ></img>
       <p>
         <a
           href="https://www.youtube.com/watch?v=eBGIQ7ZuuiU"
