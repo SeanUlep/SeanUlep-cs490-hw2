9a38b9ebe59f1c7b21c26b83e49df39e00628e50
SeanUlep <su25@njit.edu>
2023-01-27 02:21:39 -0500

    Completed Cells

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -1,11 +1,13 @@
 import { MetaTags } from '@redwoodjs/web'
 
+import ArticlesCell from 'src/components/ArticlesCell'
 const HomePage = () => {
   return (
     <>
       <MetaTags title="Home" description="Home page" />
       Home, it's where the heart is. CUZ WHAT IS LOVE, I WANNA KNOW WHAT LOVE
       ISSSS!!
+      <ArticlesCell />
     </>
   )
 }
