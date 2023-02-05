eeb20d87dac40e8d21cd66f194330b4c87d19003
SeanUlep <su25@njit.edu>
2023-02-04 23:16:32 -0500

    Finished Chapter 7!

--- api/src/services/posts/posts.js
+++ api/src/services/posts/posts.js
@@ -1,29 +1,43 @@
+import { ForbiddenError } from '@redwoodjs/graphql-server'
+
 import { db } from 'src/lib/db'
 
-export const posts = () => {
-  return db.post.findMany()
+const validateOwnership = async ({ id }) => {
+  if (await adminPost({ id })) {
+    return true
+  } else {
+    throw new ForbiddenError("You don't have access to this post")
+  }
 }
 
-export const post = ({ id }) => {
-  return db.post.findUnique({
-    where: { id },
+export const adminPosts = () => {
+  return db.post.findMany({ where: { userId: context.currentUser.id } })
+}
+
+export const adminPost = ({ id }) => {
+  return db.post.findFirst({
+    where: { id, userId: context.currentUser.id },
   })
 }
 
 export const createPost = ({ input }) => {
   return db.post.create({
-    data: input,
+    data: { ...input, userId: context.currentUser.id },
   })
 }
 
-export const updatePost = ({ id, input }) => {
+export const updatePost = async ({ id, input }) => {
+  await validateOwnership({ id })
+
   return db.post.update({
     data: input,
     where: { id },
   })
 }
 
-export const deletePost = ({ id }) => {
+export const deletePost = async ({ id }) => {
+  await validateOwnership({ id })
+
   return db.post.delete({
     where: { id },
   })
